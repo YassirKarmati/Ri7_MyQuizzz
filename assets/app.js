1f60135c855da1dc48a8b3ef8560386e6676a87a
YassirKarmati <yassirkarmati@gmail.com>
2022-11-29 17:08:33 +0100

    Création d'une version design V2
    Utilisation de bootstrap pour designer facilement le quizz
    Arrangement du JS pour plus de fonctionnalité

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,10 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Le problème d'undefined est que la boucle for continue de lire le tableau question mais arrive à [5] qui ne contient rien
+
+
+
 let questions = [
     "Géographie : Sur quel continent se situe l'Arabie Saoudite ? ",
     "Mathématiques : Combien font 6 x 7 ? ",
@@ -11,41 +15,50 @@ let questions = [
 ];
 
 let reponse = [
-    ["Afrique","Asie","Europe","Océanie"],
-    ["43","54","36","42"],
-    ["Ornithorinque","Ornitorynque","Ornythorinque","Ornithorynque"],
-    ["1809","1789","1799","1889"],
-    ["Des volcans","Des fonds marins","D'origine extra-terrestre","Sur les plages"]
+    ["Afrique", "Asie", "Europe", "Océanie"],
+    ["43", "54", "36", "42"],
+    ["Ornithorinque", "Ornitorynque", "Ornythorinque", "Ornithorynque"],
+    ["1809", "1789", "1799", "1889"],
+    ["Des volcans", "Des fonds marins", "D'origine extra-terrestre", "Sur les plages"]
 ];
-let bonneReponse = ["Asie","42","Ornithorynque","Des volcans"];
+let bonneReponse = ["Asie", "42", "Ornithorynque", "1789", "Des volcans"];
 
 let counter = 0;
-let score =0 ;
+let score = 0;
 
 function displayQuestion() {
-    document.querySelector("#question").innerHTML = questions[counter];
-    displayReponse();
+    if (questions[counter] !== undefined) {
+        document.querySelector("#question").innerHTML = questions[counter];
+        displayReponse();
+    } else {
+
+        document.querySelector("#containerQuizz").innerHTML = '<div id="containerQuizz"><h3> Vous avez terminé ! </h3> <span>Votre score est de ' + score + ' / 5. Merci d"avoir joué.</span></div>';
+    }
+
 
 }
 
 function displayReponse() {
-   let container = document.querySelectorAll(".reponse");
-   for (let i = 0; i < reponse[counter].length; i++) {
+    let container = document.querySelectorAll(".reponse");
+    for (let i = 0; i <= reponse[counter].length; i++) {
         container[i].innerHTML = reponse[counter][i];
-    
-   }
+
+    }
 }
 
 function reply(elem) {
-    if (elem.innerHTML==bonneReponse[counter]) {
-        score ++;
+    if (elem.innerHTML == bonneReponse[counter]) {
+        score++;
         document.querySelector("#score").innerHTML = score;
     }
-    counter ++;
+    counter++;
     displayQuestion();
+
 }
 
 displayQuestion();
+
+
 
 
 // Ajouter un if counter = 5 -> fin du jeu
---
+++ assets/app2.js
@@ -0,0 +1,86 @@
+// Déclaration des questions ainsi que des réponses dans des tableaux multidimensionnelles.
+// Un tableau bonneRéponse est aussi créé pour comparé si les réponses données par le joueur correspondent à la bonne réponse.
+//      Une variable score et une variable counter sont déclarées pour compter le score et 
+//      compter à quelle questions nous sommes dans l'index du tableau des questions
+//      afin de passer correctement aux questions suivantes.
+
+document.querySelector("#containerQuizz").hidden = true;
+document.querySelector("#monScore").hidden = true;
+
+let questions = [
+    "Géographie : Sur quel continent se situe l'Arabie Saoudite ? ",
+    "Mathématiques : Combien font 6 x 7 ? ",
+    "Français : Quelle est la bonne orthographe ? ",
+    "Histoire : En quelle année a eu lieu la révolution française ? ",
+    "Science : D'où provient l'obsidienne ? "
+];
+
+let reponse = [
+    ["Afrique", "Asie", "Europe", "Océanie"],
+    ["43", "54", "36", "42"],
+    ["Ornithorinque", "Ornitorynque", "Ornythorinque", "Ornithorynque"],
+    ["1809", "1789", "1799", "1889"],
+    ["Des volcans", "Des fonds marins", "D'origine extra-terrestre", "Sur les plages"]
+];
+let bonneReponse = ["Asie", "42", "Ornithorynque", "1789", "Des volcans"];
+
+function startQuizz() {
+    document.querySelector("#support").hidden = true;
+    document.querySelector("#containerQuizz").hidden = false;
+    document.querySelector("#monScore").hidden = false;
+    counter = 0;
+    score = 0;
+}
+let counter = 0;
+let score = 0;
+
+// Pour réaliser le quizz, nous avons besoin de trois fonctions pour l'instant :
+
+// function displayQuestion() servira a afficher les questions dans le HTML.
+
+// function displayReponse() servira a afficher les 4 réponses possibles dans des boutons clicables.
+// Pour cela, on utilise le querySelectorAll qui permet d'obtenir une liste d'élément (grâce au All)
+// La boucle for servira a afficher les réponses chacunes dans une question grace à l'index de reponse[counter][i]
+
+// function reply(elem) servira a vérifier si la réponse donner par le joueur correspond à la bonne réponse. Si c'est le cas : score ++.
+// Elle permet aussi de passer à la question suivante en augmentant la variable counter.
+
+function displayQuestion() {
+    if (questions[counter] !== undefined) {
+        document.querySelector("#question").innerHTML = questions[counter];
+        displayReponse();
+    } else {
+        document.querySelector("#support").hidden = false;
+        document.querySelector("#containerQuizz").hidden = true;
+        document.querySelector("#support").innerHTML = '<div class="text-center"> <h3> Vous avez terminé ! </h3> <span>Votre score est de ' + score + ' / 5. Merci d"avoir joué.</span></div>';
+        score = 0;
+        counter = 0;
+    }
+
+}
+
+function displayReponse() {
+    let container = document.querySelectorAll("#reponse");
+    for (let i = 0; i <= reponse[counter].length; i++) {
+        container[i].innerHTML = reponse[counter][i];
+    }
+}
+
+function reply(elem) {
+    if (elem.innerHTML == bonneReponse[counter]) {
+        score++;
+        document.querySelector("#score").innerHTML = score;
+    }
+    counter++;
+    displayQuestion();
+
+}
+
+displayQuestion();
+
+
+// TODO :
+// Trouver comment mettre des animations lorsque l'on passe à la question suivante
+// Ajouter une animation de félicitations (confettis)
+// Ajouter l'aléatoire dans les questions / réponses
+// Réussir à réinitialiser le jeu avec le bouton
